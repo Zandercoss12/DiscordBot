95af179486d0f0365e3011c38b5d2b42db3d4827
SularGaming <38816215+SularGaming@users.noreply.github.com>
2018-08-30 13:10:48 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ client.on("message", async message => {
   if(message.author.bot) return;
   if(message.content.indexOf(config.prefix) !== 0) return;
   
-const args = message.content.slice(config.prefix.lenght).trim().split(/ +g);
+const args = message.content.slice(config.prefix.length).trim().split(/ +g);
 const command = args.shift().toLowerCase();
 
 if(command === "info") {
