c75b54681bef9fb72737d697a9ad0e4450b6b529
SularGaming <38816215+SularGaming@users.noreply.github.com>
2018-08-31 15:02:50 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,8 +16,8 @@ const command = args.shift().toLowerCase();
 if(command === "info") {
     var embed = new Discord.RichEmbed()
       .setThumbnail(message.client.avatarURL)
-      .addField("Hello my name is ToastyCord.", true)
-      .addField("I am developed to help those who cannot help themselves.", true)
+      .addField("Hello my name is ToastyCord.",)
+      .addField("I am developed to help those who cannot help themselves.",)
       .addField("Do t!help for more commands.")
       .addField("Thanks for inviting me and enjoy.")
       .setColor(0x00FFFF)
