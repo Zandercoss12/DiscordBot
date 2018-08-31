5dc3d00267498032c122d3cb7cbf6c6f4474bacc
Zandercross12 <tuoi.nguyen69@icloud.com>
2018-08-31 08:37:20 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,30 +1,30 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const config = require("./config.json");
+const config = require('./config.json');
 
-client.on('ready',() => {
-  client.user.setPresence({game: {name: "t!help for commands!", type:0}});
-  });
-  
+client.on('ready', () => {
+  client.user.setPresence({game: {name: "t!help for commands!", type: 0}});
+});
+
 client.on("message", async message => {
-  if(message.author.bot) return;
-  if(message.content.indexOf(config.prefix) !== 0) return;
-  
-const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-const command = args.shift().toLowerCase();
+  if (message.author.bot) return;
+  if (message.content.indexOf(config.prefix) !== 0) return;
 
-if(command === "info") {
-  var embed = new Discord.RichEmbed()
-    .addField("Why am I here?!", "I want to leave!" , true)
+  const args = message.content.slice(config.prefix.length).trim.split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  if (command === "info") {
+    var embed = new Discord.RichEmbed()
+    .addField("Why am I here?!", "I want to leave!", true)
     .addField("End my life")
     .setColor(0x00FFFF)
-  message.channel.sendEmbed(embed);
-}
+    message.channel.sendEmbed(embed);
+  }
 
 });
 
 setInterval(() => {
-  http.get('http://discordjs-heroku.herokuapp.com');
+ http.get('http://discordjs-heroku.herokuapp.com');
 }, 900000);
 
 const port = process.env.PORT || 5000;
