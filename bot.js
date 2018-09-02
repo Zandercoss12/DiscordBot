0f64ee239d66508df501aba081a3ef322588cbc2
SularGaming <38816215+SularGaming@users.noreply.github.com>
2018-09-02 09:35:56 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 const config = require("./config.json");
 
 client.on('ready',() => {
-  client.user.setPresence({game: {name: "In maintenance mode!!!", type:1}});
+  client.user.setPresence({game: {name: "In maintenance mode", type:0}});
   });
   
 client.on("message", async message => {
